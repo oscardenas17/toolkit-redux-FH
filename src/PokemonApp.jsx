8834327e9394c1299c8e91677ff22bee1e6077f1
oscardenas17 <oscardenas17@gmail.com>
2022-10-10 23:09:57 -0500

    Thunks para lanzar dispatchs

--- src/PokemonApp.jsx
+++ src/PokemonApp.jsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { getPokemons } from "./store/slices/pokemon";
 
 const PokemonApp = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPokemons());
+  }, []);
+
   return (
     <>
       <h1>pokemon</h1>
       <hr />
       <ul>
-        <li></li>
-        <li></li>
-        <li></li>
+        <li>d</li>
+        <li>d</li>
+        <li>d</li>
       </ul>
     </>
   );
