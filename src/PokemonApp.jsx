7201a911d1c5853ad7a7fb6a9a81fca314552274
oscardenas17 <oscardenas17@gmail.com>
2022-10-10 22:52:37 -0500

    Configurando Slice nuevo-pokemons

---
+++ src/PokemonApp.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const PokemonApp = () => {
+  return (
+    <>
+      <h1>pokemon</h1>
+      <hr />
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </>
+  );
+};
+
+export default PokemonApp;
