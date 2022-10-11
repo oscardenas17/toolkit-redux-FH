9208ada801f08e4ad88bbff13bbe5180172a21d5
oscardenas17 <oscardenas17@gmail.com>
2022-10-11 10:14:56 -0500

    RTK query esqueleto

---
+++ src/TodoApp.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const TodoApp = () => {
+  return (
+    <>
+      <h1>Todosd- RTK Query</h1>
+      <hr />
+      <h4>Loading</h4>
+
+      <pre>...</pre>
+
+      <button>Next Todo</button>
+    </>
+  );
+};
+
+export default TodoApp;
