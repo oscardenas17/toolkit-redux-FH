04ec33c23daeb95a0411fa16ea2c18c880cdae29
oscardenas17 <oscardenas17@gmail.com>
2022-10-11 10:51:28 -0500

    Obetner un TODO por ID

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,24 +1,39 @@
-import React from "react";
-import { useGetTodosQuery } from "./store/apis";
+import { useState } from "react";
+import { useGetTodosQuery, useGetTodoQuery } from "./store/apis";
 
 const TodoApp = () => {
-  const { data: todos = [], isLoading } = useGetTodosQuery();
+  const [todoId, setTodoId] = useState(1);
+  // const { data: todos = [], isLoading } = useGetTodosQuery();
+  const { data: todo, isLoading } = useGetTodoQuery(todoId);
+
+  const nextTodo = () => {
+    setTodoId(todoId + 1);
+  };
+
+  const prevTodo = () => {
+    if (todoId === 1) return;
+    setTodoId(todoId - 1);
+  };
+
   return (
     <>
-      <h1>Todosd- RTK Query</h1>
+      <h1>Todos - RTK Query</h1>
       <hr />
-      <h4>Loading: {isLoading ? "true" : "false"}</h4>
+      <h4>isLoading: {isLoading ? "True" : "False"} </h4>
 
-      <pre>...</pre>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>
-            <strong>{todo.completed ? "DONE" : "Pending"}</strong>
-            {todo.title}
-          </li>
-        ))}
-      </ul>
-      <button>Next Todo</button>
+      <pre>{JSON.stringify(todo)}</pre>
+
+      <button onClick={prevTodo}>Prev Todo</button>
+      <button onClick={nextTodo}>Next Todo</button>
+      {/* 
+        <ul>
+            { todos.map( todo => (
+                <li key={ todo.id }>
+                    <strong> { todo.completed ? 'DONE' : 'Pending' } </strong> 
+                    { todo.title }
+                </li>
+            ) ) }
+        </ul> */}
     </>
   );
 };
